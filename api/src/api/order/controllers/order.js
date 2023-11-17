1d54cf01f38ca4cb32e6c962cd2f86ab96cb5109
Francia-bdz <francia.bidzimou@mmibordeaux.com>
2023-11-17 11:38:02 +0100

    feat : add order in database

---
+++ api/src/api/order/controllers/order.js
@@ -0,0 +1,12 @@
+
+// const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+'use strict';
+
+/**
+ * order controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order.order');
