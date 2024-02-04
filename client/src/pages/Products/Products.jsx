a3c23b5b10c120cee40abfafde3cbbf3db4b56e3
ODrimm <ystos.gaming@gmail.com>
2024-02-04 12:39:43 +0100

    feat : category selected

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -4,15 +4,42 @@ import List from "../../components/List/List.jsx";
 import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const Products = () => {
 
-    const [categoryTitle, setCategoryTitle] = useState("All");
 
     const { data, loading, error } = useFetch(`/categories?populate=*`);
 
     const [clickedCategory, setClickedCategory] = useState("All");
+
+    console.log(clickedCategory);
+
+    const All = useRef(null);
+    const Tshirt = useRef(null);
+    const Hoodies = useRef(null);
+
+    useEffect(() => {
+        All.current.classList.add("active");
+        if(data){
+            if (clickedCategory === "All") {
+                Hoodies.current.classList.remove("active");
+                Tshirt.current.classList.remove("active");
+            } else if (clickedCategory === "T-shirts") {
+                Tshirt.current.classList.add("active");
+                All.current.classList.remove("active");
+                Hoodies.current.classList.remove("active");
+            } else if (clickedCategory === "Hoodies") {
+                Hoodies.current.classList.add("active");
+                Tshirt.current.classList.remove("active");
+                All.current.classList.remove("active");
+            }
+        }
+        
+    }, [clickedCategory]);
+    
+
+   
 
     const renderCategories = () => {
         if (loading) {
@@ -24,9 +51,17 @@ const Products = () => {
         }
 
         if (data) {
+
             return data.map((category, index) => {
+                let ref;
+                if (category.attributes.title === "T-shirts") {
+                   ref = Tshirt;
+                } else if (category.attributes.title === "Hoodies") {
+                    ref = Hoodies;
+                }
+
                 return (
-                    <div className="products__banner__filter__item" key={index} onClick={() => setCategoryTitle(category.attributes.title) + setClickedCategory(category.attributes.title)}>
+                    <div ref={ref} className="products__banner__filter__item" key={index} onClick={() => setClickedCategory(category.attributes.title)}>
                         <img src={import.meta.env.VITE_UPLOAD_URL + category.attributes.image.data.attributes.url} alt="Dessin t-shirt" />
                         <p>{category.attributes.title}</p>
                     </div>
@@ -37,13 +72,15 @@ const Products = () => {
         return null;
     }
 
+
+    
     return (
         <div className="products">
             <div className="products__banner">
                 <h1>Les produits Groomit</h1>
                 <h2>Pour toi + moi + tous ceux qui en veulent</h2>
                 <div className="products__banner__filter">
-                    <div className="products__banner__filter__item"  onClick={() => setCategoryTitle("All") + setClickedCategory("All")}>
+                    <div ref={All} className="products__banner__filter__item"  onClick={() => setClickedCategory("All")}>
                         <img src="/img/show_all.svg" />
                         <p>Afficher tout</p>
                     </div>
@@ -54,7 +91,7 @@ const Products = () => {
 
             </div>
             <div className="products__item__row">
-                <List categoryTitle={categoryTitle} />
+                <List categoryTitle={clickedCategory} />
             </div>
 
         </div>
